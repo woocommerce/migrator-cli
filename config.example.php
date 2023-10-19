ba9f40b72c83112c1a635f1a0252cb489c6dfd7f
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2023-10-19 14:49:10 -0300

    Removed unecessary skio token from config file

--- config.example.php
+++ config.example.php
@@ -1,4 +1,3 @@
 <?php
 const ACCESS_TOKEN = 'shpat_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 const SHOPIFY_DOMAIN = 'your-store.myshopify.com';
-const SKIO_TOKEN = '';//If site uses SKIO for subscriptions
